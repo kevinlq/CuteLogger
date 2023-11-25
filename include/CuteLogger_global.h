51e3468c671856100ba6812b792bb77b55e2ad36
kevinlq <kevinlq0912@163.com>
2023-11-25 18:10:49 +0800

    feat: adaptation static and dynamic build link

--- include/CuteLogger_global.h
+++ include/CuteLogger_global.h
@@ -3,10 +3,14 @@
 
 #include <QtCore/qglobal.h>
 
-#if defined(CUTELOGGER_LIBRARY)
-#  define CUTELOGGERSHARED_EXPORT Q_DECL_EXPORT
+#if defined(QT_SHARED) || !defined(QT_STATIC)
+#  if defined(CUTELOGGER_LIBRARY)
+#    define CUTELOGGERSHARED_EXPORT Q_DECL_EXPORT
+#  else
+#    define CUTELOGGERSHARED_EXPORT Q_DECL_IMPORT
+#  endif
 #else
-#  define CUTELOGGERSHARED_EXPORT Q_DECL_IMPORT
+#  define CUTELOGGERSHARED_EXPORT
 #endif
 
 #endif // CUTELOGGER_GLOBAL_H
